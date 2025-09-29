dfe0d38b1d6049a350fb7faba6ea6033f06f698c
Chuong Tran <khanhchuong@gmail.com>
2025-09-29 11:53:15 +0200

    change file env, creer file env local

---
+++ DBrestart.bat
@@ -0,0 +1,10 @@
+@echo off
+echo Ce script efface la DB, la reinitialise et la remplit avec des donnees de test (fixtures)
+
+symfony console doctrine:database:drop --force
+symfony console doctrine:database:create
+del migrations\Ve*
+symfony console make:migration --no-interaction
+symfony console doctrine:migration:migrate --no-interaction
+@REM symfony console doctrine:fixtures:load --no-interaction
+

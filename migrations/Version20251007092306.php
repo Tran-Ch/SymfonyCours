35f3e628664118e4392a7b348b4c8b78c179d4e3
Chuong Tran <khanhchuong@gmail.com>
2025-10-07 11:29:29 +0200

    creer folder assets

--- migrations/Version20251007092306.php
+++ migrations/Version20251007092306.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250930133223 extends AbstractMigration
+final class Version20251007092306 extends AbstractMigration
 {
     public function getDescription(): string
     {

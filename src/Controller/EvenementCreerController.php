220e5feacb5f3bb78987dda78a77e40c8dfd9604
Tran-Ch <khanhchuong@gmail.com>
2025-11-18 11:56:52 +0100

    change les images

--- src/Controller/EvenementCreerController.php
+++ src/Controller/EvenementCreerController.php
@@ -1,236 +1,169 @@
 <?php
- 
+
 namespace App\Controller;
- 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
 use Symfony\Component\HttpFoundation\Response;
-
 use Symfony\Component\HttpFoundation\Request;
-
 use Symfony\Component\Routing\Attribute\Route;
-
 use Doctrine\ORM\EntityManagerInterface;
-
 use App\Entity\Evenement;
-
 use App\Form\EvenementType;
-
 use Symfony\Component\HttpFoundation\File\Exception\FileException;
-
 use Symfony\Component\HttpFoundation\File\UploadedFile;
-
 use Symfony\Component\String\Slugger\SluggerInterface;
- 
+
 final class EvenementCreerController extends AbstractController
-
 {
-
     #[Route('/evenement/creer', name: 'app_evenement_creer')]
-
-    public function index(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
-
-    {
-
+    public function index(
+        Request $request,
+        EntityManagerInterface $entityManager,
+        SluggerInterface $slugger
+    ): Response {
         $evenement = new Evenement();
-
         $form = $this->createForm(EvenementType::class, $evenement);
-
         $form->handleRequest($request);
- 
+
         if ($form->isSubmitted() && $form->isValid()) {
-
             // Gestion de l'upload d'image
-
             /** @var UploadedFile $imageFile */
-
             $imageFile = $form->get('image')->getData();
- 
+
             if ($imageFile) {
-
                 $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
-
                 $safeFilename = $slugger->slug($originalFilename);
-
                 $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
- 
+
                 try {
-
+                    // => public/Image
                     $imageFile->move(
-
-                        'uploads/images',
-
+                        $this->getParameter('kernel.project_dir') . '/public/Image',
                         $newFilename
-
                     );
-
                 } catch (FileException $e) {
-
                     $this->addFlash('error', 'Erreur lors de l\'upload de l\'image');
-
                 }
- 
-                $evenement->setImage($newFilename);
 
+                $evenement->setImage($newFilename);
             }
- 
+
             $entityManager->persist($evenement);
-
             $entityManager->flush();
- 
+
             $this->addFlash('success', 'Événement créé avec succès !');
- 
+
             return $this->redirectToRoute('app_evenement_liste');
-
         }
- 
+
         return $this->render('evenement_creer/index.html.twig', [
-
             'controller_name' => 'EvenementCreerController',
-
             'form' => $form->createView(),
-
         ]);
-
     }
- 
+
     #[Route('/evenement/liste', name: 'app_evenement_liste')]
-
     public function liste(EntityManagerInterface $entityManager): Response
-
     {
-
         $evenements = $entityManager->getRepository(Evenement::class)->findAll();
- 
+
         return $this->render('evenement_creer/liste.html.twig', [
-
-            'evenements' => $evenements,  // ← Important : cette variable doit être passée
-
+            'evenements' => $evenements,
         ]);
-
     }
- 
+
     #[Route('/evenement/{id}', name: 'app_evenement_show', requirements: ['id' => '\d+'])]
-
     public function show(int $id, EntityManagerInterface $entityManager): Response
-
     {
-
         $evenement = $entityManager->getRepository(Evenement::class)->find($id);
- 
+
         if (!$evenement) {
-
             throw $this->createNotFoundException('L\'événement demandé n\'existe pas.');
-
         }
- 
+
         return $this->render('evenement_creer/show.html.twig', [
-
             'evenement' => $evenement,
-
         ]);
-
     }
- 
+
     #[Route('/evenement/{id}/edit', name: 'app_evenement_edit', requirements: ['id' => '\d+'])]
-
-    public function edit(int $id, Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
-
-    {
-
+    public function edit(
+        int $id,
+        Request $request,
+        EntityManagerInterface $entityManager,
+        SluggerInterface $slugger
+    ): Response {
         $evenement = $entityManager->getRepository(Evenement::class)->find($id);
- 
+
         if (!$evenement) {
-
             throw $this->createNotFoundException('L\'événement demandé n\'existe pas.');
-
         }
- 
+
         $form = $this->createForm(EvenementType::class, $evenement);
-
         $form->handleRequest($request);
- 
+
         if ($form->isSubmitted() && $form->isValid()) {
-
             // Gestion de l'upload d'image
-
             /** @var UploadedFile $imageFile */
-
             $imageFile = $form->get('image')->getData();
- 
+
             if ($imageFile) {
-
                 $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
-
                 $safeFilename = $slugger->slug($originalFilename);
-
                 $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
- 
+
                 try {
-
+                    // => public/Image
                     $imageFile->move(
-
-                        'uploads/images',
-
+                        $this->getParameter('kernel.project_dir') . '/public/Image',
                         $newFilename
-
                     );
-
                 } catch (FileException $e) {
-
                     $this->addFlash('error', 'Erreur lors de l\'upload de l\'image');
-
                 }
- 
+
                 $evenement->setImage($newFilename);
-
             }
- 
+
             $entityManager->flush();
- 
+
             $this->addFlash('success', 'Événement modifié avec succès !');
- 
-            return $this->redirectToRoute('app_evenement_show', ['id' => $evenement->getId()]);
 
+            return $this->redirectToRoute('app_evenement_show', [
+                'id' => $evenement->getId(),
+            ]);
         }
- 
+
         return $this->render('evenement_creer/edit.html.twig', [
-
             'form' => $form->createView(),
-
             'evenement' => $evenement,
-
         ]);
-
     }
- 
-    #[Route('/evenement/{id}/delete', name: 'app_evenement_delete', requirements: ['id' => '\d+'], methods: ['POST'])]
 
-    public function delete(int $id, Request $request, EntityManagerInterface $entityManager): Response
-
-    {
-
+    #[Route(
+        '/evenement/{id}/delete',
+        name: 'app_evenement_delete',
+        requirements: ['id' => '\d+'],
+        methods: ['POST']
+    )]
+    public function delete(
+        int $id,
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response {
         $evenement = $entityManager->getRepository(Evenement::class)->find($id);
- 
+
         if (!$evenement) {
-
             throw $this->createNotFoundException('L\'événement demandé n\'existe pas.');
-
         }
- 
+
         if ($this->isCsrfTokenValid('delete'.$evenement->getId(), $request->request->get('_token'))) {
-
             $entityManager->remove($evenement);
-
             $entityManager->flush();
- 
+
             $this->addFlash('success', 'Événement supprimé avec succès !');
-
         }
- 
+
         return $this->redirectToRoute('app_evenement_liste');
-
     }
-
 }
- 

5451606f4cfe97605fc67559be84d4954bb26687
Tran-Ch <khanhchuong@gmail.com>
2025-11-26 15:59:06 +0100

    changement

--- src/Controller/NordController.php
+++ src/Controller/NordController.php
@@ -10,14 +10,14 @@ use Symfony\Component\Routing\Attribute\Route;
 final class NordController extends AbstractController
 {
     // Trang 1: Expérience du Nord (map vùng Bắc + nút đi tiếp)
-    #[Route('/templates/experience/nord', name: 'app_nord')]
+    #[Route('/experience/nord', name: 'app_nord')]
     public function index(): Response
     {
         return $this->render('experience/nord/index.html.twig');
     }
 
     // Trang 2: EXPÉRIENCE INCROYABLE DU NORD
-    #[Route('/templates/experience/nord/experience-incroyable', name: 'app_nord_incroyable')]
+    #[Route('/experience/nord/experience-incroyable', name: 'app_nord_incroyable')]
     public function incroyable(ExperienceSpotRepository $spotRepository): Response
     {
         // Cách 1: dùng custom method
@@ -35,14 +35,14 @@ final class NordController extends AbstractController
     }
 
     // Trang 3: DÉCOUVREZ LA CULTURE DU NORD
-    #[Route('/templates/experience/nord/culture', name: 'app_nord_culture')]
+    #[Route('/experience/nord/culture', name: 'app_nord_culture')]
     public function culture(): Response
     {
         return $this->render('experience/nord/culture.html.twig');
     }
 
     // Trang 4: TOURISME DE VACANCES DU NORD
-    #[Route('/templates/experience/nord/tourisme', name: 'app_nord_tourisme')]
+    #[Route('/experience/nord/tourisme', name: 'app_nord_tourisme')]
     public function tourisme(): Response
     {
         return $this->render('experience/nord/tourisme.html.twig');
